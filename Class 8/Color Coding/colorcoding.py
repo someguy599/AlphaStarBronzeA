35d52dfc2861d076f5f1d3a2bae69490f3c4db2a
someguy599 <119460518+someguy599@users.noreply.github.com>
2023-11-20 13:48:06 -0800

    Update colorcoding.py

--- Class 8/Color Coding/colorcoding.py
+++ Class 8/Color Coding/colorcoding.py
@@ -1,13 +1,23 @@
 N, M = map(int, input("").split(" "))
-sequences = []
-indices = {}
+sequencesone = []
+sequencestwo = []
+indices = 0
 
-for i in range(N*2): sequences.append(input(""))
+for i in range(N): sequencesone.append(input(""))
+for j in range(N): sequencestwo.append(input(""))
 
-for index in range(0, N):
-    info = []
-    for sequence in sequences:
-        if sequence[index] not in info: info.append(sequence[index])
-    indices[index] = info
+for index in range(0, M):
+    infoone = []
+    infotwo = []
+    common = 0
+    for sequence in sequencesone:
+        if sequence[index] not in infoone: infoone.append(sequence[index])
+    for sequence in sequencestwo:
+        if sequence[index] not in infotwo: infotwo.append(sequence[index])
+        
+    for letter in infoone:
+        if letter in infotwo: common += 1
+    
+    if common == 0: indices += 1
 
 print(indices)
