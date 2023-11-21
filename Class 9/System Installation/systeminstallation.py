04ffeee94745b6c9e677933d79f1a2471c46c1a1
someguy599 <119460518+someguy599@users.noreply.github.com>
2023-11-21 15:50:54 -0800

    Update systeminstallation.py

--- Class 9/System Installation/systeminstallation.py
+++ Class 9/System Installation/systeminstallation.py
@@ -7,14 +7,14 @@ coord = (0, 0)
 for i in range(NR):
     grid.append(list(map(int, input("").split(" "))))
 
-for row in range(NR - 2):
-    for column in range(NC - 2):
-        row1 = grid[row][column] + grid[row][column + 1] + grid[row][column + 2]
-        row2 = grid[row + 1][column] + grid[row + 1][column + 1] + grid[row + 1][column + 2]
-        row3 = grid[row + 2][column] + grid[row + 2][column + 1] + grid[row + 2][column + 2]
+for row in range(NR-2):
+    for column in range(NC-2):
+        row1 = grid[row][column] + grid[row][column+1] + grid[row][column+2]
+        row2 = grid[row + 1][column] + grid[row + 1][column+1] + grid[row + 1][column+2]
+        row3 = grid[row + 2][column] + grid[row + 2][column+1] + grid[row + 2][column+2]
         if row1 + row2 + row3 > max:
             max = row1 + row2 + row3
-            coord = (row + 1, column + 1)
-
+            coord = (row+1, column+1)
+        
 print(max)
 print(coord[0], coord[1])
