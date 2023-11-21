c3cf01e879f92a664009d8b693baf6c2fc7ba1a7
someguy599 <119460518+someguy599@users.noreply.github.com>
2023-11-21 15:52:31 -0800

    Update discow.py

--- Class 9/Discow/discow.py
+++ Class 9/Discow/discow.py
@@ -10,11 +10,10 @@ for letter in range(len(paths)):
             if paths[letter] == paths[a]:
                 finalindice = a
                 break
+            
         observed = paths[letter:finalindice+1]
         if len(observed) > 2:
             for character in observed:
-                if observed.count(character) == 1:
-                    intersections += 1
+                if observed.count(character) == 1: intersections += 1
+                    
 print(intersections//2)
-
-
